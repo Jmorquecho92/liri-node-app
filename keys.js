db679e0ac4363f9e32d920a3fc61e6ae5efa8636
Jmorquecho92 <morquecho.jesus@yahoo.com>
2017-11-03 17:22:58 -0700

    update keys.js

---
+++ keys.js
@@ -0,0 +1,17 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+	consumer_key:'EGssghCxtPTXPeq2I8nlsNCO9',
+	consumer_secret:"N72LcIcWAPBO5cSbqUiGGqxZ5ZUdF9L0GOX03GlyFR64ZO7Cgp",
+	access_token_key: '926220398764744704-RCl8Iev5OEYGnlu9xzRUt23PKSpqUz3',
+	access_token_secret: '3d4rRHkQ2e1Gm5VlRS4QlBwzXJzXVwAdNSCJAOkARD3iT',
+};
+
+module.exports = twitterKeys;
+
+exports.spotifyKeys = {
+	id: 'c3153212fceb40eba424cf025e1ee26e',
+	secret: '28c7f8680ba84a11b2a19757e0d7c498',
+};
+
+exports.omdbkey = "40e9cece";
